5f96801d590e77741215e7a3baf8f4ee32dd5e0e
ChuckTheBoss <collin.mathews@gmail.com>
2023-03-01 16:55:22 -0700

    Add apples incrementing by 1/sec. Add a basket which will gather growing apples on click

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
 
+
+function Bank() {
+    this.apples = 0;
+    this.clickTick = () => {
+        this.apples += multiplier;
+        document.querySelector(".appleBank").innerText = this.apples;
+    }
+    let multiplier = 1;
+}
+let bank = new Bank();
+
+document.querySelector(".appleBank").addEventListener("click", bank.clickTick);
+
+let interval = 1000;
+
+setInterval(bank.clickTick, interval);
+
+function Basket() {
+    this.apples = 0;
+    this.gather = () => {
+        this.apples += bank.apples;
+        document.querySelector(".basket").innerText = this.apples;
+        bank.apples = 0;
+        document.querySelector(".appleBank").innerText = bank.apples;
+    }
+}
+
+let basket = new Basket();
+document.querySelector(".basket").addEventListener("click", basket.gather);
