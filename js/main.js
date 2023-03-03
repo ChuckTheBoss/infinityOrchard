99b8b95f0fa2a5747407677009539fad7d6ccd09
ChuckTheBoss <collin.mathews@gmail.com>
2023-03-03 13:52:08 -0700

    update objects to not use constructors

--- js/main.js
+++ js/main.js
@@ -1,30 +1,31 @@
 
 
-function Bank() {
-    this.apples = 0;
-    this.clickTick = () => {
-        this.apples += multiplier;
-        document.querySelector(".appleBank").innerText = this.apples;
-    }
-    let multiplier = 1;
-}
-let bank = new Bank();
+let bank = {
+    apples: 0,
+    name: "margaret",
+    clickTick() {
+        bank.apples += bank.multiplier;
+        document.querySelector(".appleBank").innerText = bank.apples;
+        return bank.apples;
+    },
+    multiplier: 1,
+};
 
 document.querySelector(".appleBank").addEventListener("click", bank.clickTick);
+console.log(bank.apples, bank.multiplier, bank.clickTick())
 
 let interval = 1000;
 
 setInterval(bank.clickTick, interval);
 
-function Basket() {
-    this.apples = 0;
-    this.gather = () => {
-        this.apples += bank.apples;
-        document.querySelector(".basket").innerText = this.apples;
+let basket = {
+    apples: 0,
+    gather() {
+        basket.apples += bank.apples;
+        document.querySelector(".basket").innerText = basket.apples;
         bank.apples = 0;
         document.querySelector(".appleBank").innerText = bank.apples;
-    }
-}
+    },
+};
 
-let basket = new Basket();
 document.querySelector(".basket").addEventListener("click", basket.gather);
